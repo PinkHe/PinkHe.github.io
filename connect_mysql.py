9a2e3402eea75b7a8678fbf676a4de8c7efad2d2
PinkHe <762819320@qq.com>
2020-08-19 19:20:31 +0800

    获取中文字段首字母

--- connect_mysql.py
+++ connect_mysql.py
@@ -1,4 +1,5 @@
 import pymysql
+import uuid
 host="localhost"
 username="root"
 password="123456"
@@ -8,10 +9,31 @@ connect=pymysql.connect(host,username,password,db_name)
 #获取游标对象
 cursor=connect.cursor()
 
-select_sql = "select * from student"
+select_all_sql = "select * from gmtable"
+#批量插入数据
+'''
+resouces = [
+    ("2","2","2","2","2","2"),
+    ("3","3","3","3","3","3"),
+    ("4","4","4","4","4","4"),
+]
 
+insert_sql = "insert into gmtable(Gmid,GmName,GmInfo,Gmbody,GmExample,GmExampleInfo) \
+    values(%s, %s, %s, %s, %s, %s)"
+cursor.executemany(insert_sql,resouces)
+connect.commit()
+'''
+print("请输入Gm名称")
 
-cursor.execute(select_sql)
+print("请输入Gm描述")
+
+print("请输入Gm命令")
+
+print("请输入Gm举例")
+
+print("请输入举例描述")
+
+cursor.execute(select_all_sql)
 #result=cursor.fetchone()
 
 result=cursor.fetchall()
