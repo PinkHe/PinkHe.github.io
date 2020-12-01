575a9b6e7a5e8af47aad9b79d14132d868debf05
PinkHe <762819320@qq.com>
2020-12-01 19:27:14 +0800

    django

--- JobWeb/train_query/urls.py
+++ JobWeb/train_query/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.find_company_by_company_name, name = 'find_company_by_company_name'),
 ]
--- JobWeb/train_query/views.py
+++ JobWeb/train_query/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Django")
+
+def find_company_by_company_name(request, input_company_name):#, input_company_name
+    response = "输入的公司名称为 %s"#%s
+    return HttpResponse(response % input_company_name)#% input_company_name
