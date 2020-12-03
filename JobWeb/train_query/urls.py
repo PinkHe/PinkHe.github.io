510eb735b9fa3c57d33bfced6572f0fd002ac13e
PinkHe <762819320@qq.com>
2020-12-03 20:36:24 +0800

    django  通用视图

--- JobWeb/train_query/urls.py
+++ JobWeb/train_query/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'train_company'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:input_company_id>/', views.find_company_by_company_id, name = 'find_company_by_company_id'),
-    path('<int:input_company_name>/', views.find_company_by_company_name, name = 'find_company_by_company_name'),
-    path('<int:input_position_id>/', views.find_position_by_position_id, name = 'find_position_by_position_id'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'find_company_by_company_id'),
+    # path('<int:pk>/', views.find_company_by_company_name, name = 'find_company_by_company_name'),
+    # path('position/<int:pk>/', views.find_position_by_position_id, name = 'find_position_by_position_id'),
+    # path('position_commit/<int:temp_position_id>/', views.commit_resume, name = 'commit_resume'),
    
 ]
--- JobWeb/train_query/views.py
+++ JobWeb/train_query/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404
+from django.views import generic
+
+
+
+
 from .models import TrainCompany, WorkPosition
-
-
+'''
 # Create your views here.
 def index(request):
     company_list = TrainCompany.objects.all()
@@ -14,17 +19,17 @@ def index(request):
     print(context)
     return HttpResponse(template.render(context, request))
 
-def find_company_by_company_name(request, input_company_name):#, input_company_name
+def find_company_by_company_name(request, pk):#, input_company_name
     response = "输入的公司名称为 %s"#%s
-    return HttpResponse(response % input_company_name)#% input_company_name
+    return HttpResponse(response % pk)#% input_company_name
 
-def find_company_by_company_id(request, input_company_id):#, input_company_name   
+def find_company_by_company_id(request, pk):#, input_company_name   
     company_info_list = TrainCompany.objects.all()
-    if input_company_id !='' and input_company_id !=None:
+    if pk !='' and pk !=None:
         for i in company_info_list:
-            if input_company_id == i.id:
+            if pk == i.id:
                 company_info = i   
-    work_position_list = WorkPosition.objects.filter(train_company__id=company_info.id)
+    work_position_list = WorkPosition.objects.filter(pk=company_info.id)
     response = "输入的公司信息为 %s"#%s
     company_info_text = company_info.train_company_name + company_info.train_company_address_province + company_info.train_company_address_city + company_info.train_company_address_area + company_info.train_company_address_info
     context = {
@@ -35,5 +40,28 @@ def find_company_by_company_id(request, input_company_id):#, input_company_name
     return render(request, 'train_query/company_info.html',context)
 
 
-def find_position_by_position_id(request, input_position_id):
-    return render(request, 'train_query/position_info.html', context='pass')
+def find_position_by_position_id(request, pk):
+    # temp_position = WorkPosition.objects.get(id=input_position_id)
+    temp_position = get_object_or_404(WorkPosition, id=pk)
+
+    return render(request, 'train_query/position_info.html', context={'temp_position':temp_position,})
+'''
+def commit_resume(request, temp_position_id):
+    personal_info = request.POST['personal_info_name']
+    return render(request, 'train_query/result.html',context={'result':personal_info,})   
+
+
+class IndexView(generic.ListView):
+    template_name = 'train_query/index.html'
+    context_object_name = 'company_list'
+    def get_queryset(self):
+        return TrainCompany.objects.all()
+
+class DetailView(generic.ListView):
+    model = TrainCompany
+    template_name = 'train_query/company_info.html'
+    context_object_name = 'work_position_list'
+    
+    def get_queryset(self):
+        print(self.kwargs[0])
+        return WorkPosition.objects.filter(pk=self.kwargs[0])    
