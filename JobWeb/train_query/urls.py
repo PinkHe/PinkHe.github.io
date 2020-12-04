f8bb8a16c0f5fe8a70eeba3d16dc6a17becf8dd9
PinkHe <762819320@qq.com>
2020-12-04 19:54:20 +0800

    django

--- JobWeb/train_query/urls.py
+++ JobWeb/train_query/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 app_name = 'train_company'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name = 'find_company_by_company_id'),
-    # path('<int:pk>/', views.find_company_by_company_name, name = 'find_company_by_company_name'),
-    # path('position/<int:pk>/', views.find_position_by_position_id, name = 'find_position_by_position_id'),
-    # path('position_commit/<int:temp_position_id>/', views.commit_resume, name = 'commit_resume'),
+    path('', views.index, name='index'),
+    path('<int:pk>/', views.find_company_by_company_id, name = 'find_company_by_company_id'),
+    path('<int:pk>/', views.find_company_by_company_name, name = 'find_company_by_company_name'),
+    path('position/<int:pk>/', views.find_position_by_position_id, name = 'find_position_by_position_id'),
+    path('position_commit/<int:temp_position_id>/', views.commit_resume, name = 'commit_resume'),
+    path('train_company_search/', views.TrainCompanySearchView.as_view(), name = 'train_company_search'),
    
 ]
--- JobWeb/train_query/views.py
+++ JobWeb/train_query/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views import generic
 
 
 
 
 from .models import TrainCompany, WorkPosition
-'''
+
 # Create your views here.
+#培训公司查询视图
+SEARCH_TYPE_DICT = {
+    'name':'1',
+    'address':'2',
+}
+
+# def train_company_search(request):
+#     TrainCompany
+class TrainCompanySearchView(generic.ListView):
+    template_name = 'train_query/index.html'
+    context_object_name = 'company_list'
+    
+    def get_queryset(self):
+        if request.POST['search_value'] ==None or request.POST['search_value'] == '':
+            return 'is Error'
+        if request.POST['search_value'] == SEARCH_TYPE_DICT['name']:
+            print("按培训名称查询")
+        elif request.POST['search_value'] == SEARCH_TYPE_DICT['address']:
+            print("按培训地址查询")
+        return TrainCompany.objects.all()       
+# def TrainCompanySearchView(request):
+#     if request.POST['search_value'] ==None or request.POST['search_value'] == '':
+#         return 'is Error'
+#     if request.POST['search_value'] == SEARCH_TYPE_DICT['name']:
+#         print("按培训名称查询")
+#     elif request.POST['search_value'] == SEARCH_TYPE_DICT['address']:
+#         print("按培训地址查询")
+#     company_list = TrainCompany.objects.all()
+#     template = loader.get_template('train_query/index.html')
+#     context = {
+#         'company_list':company_list,
+#     }
+#     print(context)
+#     return HttpResponse(template.render(context, request))
+
+#
 def index(request):
     company_list = TrainCompany.objects.all()
     template = loader.get_template('train_query/index.html')
@@ -29,7 +65,7 @@ def find_company_by_company_id(request, pk):#, input_company_name
         for i in company_info_list:
             if pk == i.id:
                 company_info = i   
-    work_position_list = WorkPosition.objects.filter(pk=company_info.id)
+    work_position_list = WorkPosition.objects.filter(pk=company_info.id + 1 )
     response = "输入的公司信息为 %s"#%s
     company_info_text = company_info.train_company_name + company_info.train_company_address_province + company_info.train_company_address_city + company_info.train_company_address_area + company_info.train_company_address_info
     context = {
@@ -37,7 +73,7 @@ def find_company_by_company_id(request, pk):#, input_company_name
         'work_position_list':work_position_list,
     }
     # return HttpResponse(response % context)#% input_company_name
-    return render(request, 'train_query/company_info.html',context)
+    return render(request, 'train_query/company_info.html',context)   
 
 
 def find_position_by_position_id(request, pk):
@@ -45,23 +81,26 @@ def find_position_by_position_id(request, pk):
     temp_position = get_object_or_404(WorkPosition, id=pk)
 
     return render(request, 'train_query/position_info.html', context={'temp_position':temp_position,})
-'''
+
 def commit_resume(request, temp_position_id):
     personal_info = request.POST['personal_info_name']
     return render(request, 'train_query/result.html',context={'result':personal_info,})   
 
 
-class IndexView(generic.ListView):
-    template_name = 'train_query/index.html'
-    context_object_name = 'company_list'
-    def get_queryset(self):
-        return TrainCompany.objects.all()
 
-class DetailView(generic.ListView):
-    model = TrainCompany
-    template_name = 'train_query/company_info.html'
-    context_object_name = 'work_position_list'
+#替换为通用视图 
+# 
+# class IndexView(generic.ListView):
+#     template_name = 'train_query/index.html'
+#     context_object_name = 'company_list'
+#     def get_queryset(self):
+#         return TrainCompany.objects.all()
+
+# class DetailView(generic.ListView):
+#     model = TrainCompany
+#     template_name = 'train_query/company_info.html'
+#     context_object_name = 'work_position_list'
     
-    def get_queryset(self):
-        print(self.kwargs[0])
-        return WorkPosition.objects.filter(pk=self.kwargs[0])    
+#     def get_queryset(self):
+#         print(self.kwargs[0])
+#         return WorkPosition.objects.filter(pk=self.kwargs[0])    
