a89601b0724d82b9a13c3dbc565bc65e2985cba1
PinkHe <762819320@qq.com>
2021-01-05 21:07:43 +0800

    1

--- TestProtest/chepiaoDemo/main.py
+++ TestProtest/chepiaoDemo/main.py
@@ -1,6 +1,15 @@
 import requests
+import re
+import base64
+from PIL import Image
+# from os import BytesIO
 
-re = requests.get("https://kyfw.12306.cn/otn/resources/login.html")
+response = requests.get("https://kyfw.12306.cn/otn/resources/login.html")
+erweima_image_base64_code = re.findall(r'<img id="J-qrImg" src="data:image/jpg;base64,(.*?)">', response.text)
+print(erweima_image_base64_code)
+code = base64.b64decode(erweima_image_base64_code[0])
+with open("1.jpg",wb) as file:
+    file.write(code)
 
 
 print(re.text)
